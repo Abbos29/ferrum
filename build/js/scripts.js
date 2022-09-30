81c384dcec6dbb72efe5fa89f16a46a62612ef57
abbos <abbosvaliev2003@gmail.com>
2022-09-30 12:31:12 +0500

    feat: complete all 7 pages

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -50,3 +50,38 @@ function fixedNav() {
 }
 window.addEventListener('scroll', fixedNav)
 
+
+
+
+
+// swiper (info)
+
+const swiper = new Swiper('.info__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+
+    autoplay: {
+        delay: 4000,
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  //   // Responsive breakpoints
+  //   breakpoints: {
+  //   // when window width is >= 320px
+  //   320: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20
+  //   },
+  //   // when window width is >= 480px
+  //   480: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 30
+  //   },
+  // }
+});
